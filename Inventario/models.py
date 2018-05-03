bb415ff963080446e13029158c4574276ed24970
Sttp92 <esteban.estay@ing.uchile.cl>
2018-05-03 16:53:51 -0400

    Base de datos

---
+++ Inventario/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=50)
+    rut=models.PositiveIntegerField
+    digitoVerificador=models.PositiveIntegerField
+    correo=models.EmailField(max_length=60)
+    password=models.CharField(max_length=200)
+    esAdmin=models.BooleanField
+
+class Reserva(models.Model):
+    id=models.PositiveIntegerField
+    nombre=models.CharField(max_length=100)
+    horaInicio=models.TimeField
+    horaTermino=models.TimeField
+    solicitante=Usuario
+    fechaReserva=models.DateField
+    fechaCreacion=models.DateTimeField
+
+class Prestamo(models.Model):
+    id = models.PositiveIntegerField
+    nombre = models.CharField(max_length=100)
+    horaInicio = models.TimeField
+    horaTermino = models.TimeField
+    solicitante = Usuario
+    fechaPrestamo = models.DateField
+    estado=(('V','Vigente'),('P','Perdido'),('C','Caducado'))
+
+class Articulo(models.Model):
+    nombre=models.CharField(max_length=50)
+    id=models.CharField(max_length=100)
+    estado=(('D','Disponible'),('P','En prestamo'),('R','En reparacion'),('P','Perdido'))
+    descripcion=models.CharField(max_length=500)
+    foto=models.ImageField
+
+class Espacio(models.Model):
+    nombre=models.CharField(max_length=50)
+    id=models.CharField(max_length=100)
+    estado=(('D','Disponible'),('P','En prestamo'),('R','En reparacion'))
+    descripcion=models.CharField(max_length=500)
+    foto=models.ImageField
+    capacidad=models.PositiveIntegerField
---
+++ Inventario/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def landingPageUsuario(request):
+    return HttpResponse("hola")
+
+def landingPageAdministrador(request):
+    pass
+
+def registerPage(request):
+    pass
+
+def iniciarSesion(request):
+    pass
