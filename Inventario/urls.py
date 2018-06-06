0d97b8c0a03e16eb70ec7586b907c20098e9f661
Sttp92 <esteban.estay@ing.uchile.cl>
2018-06-06 00:29:27 -0400

    Se modificaron los archivos landingPageUsuario.html y urls (aparecen los articulos como botones despues de la busqueda y llevan a la ficha del articulo)

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('perfil/', views.perfilUsuario, name='perfilUsuario'),
     path('articulo/<int:articuloId>/', views.fichaArticulo, name='fichaArticulo'),
     path('lpadmin/', views.landingPageAdministrador, name='lpAdmin'),
-    path('lpusuario/', views.landingPageUsuario, name='lpUsuario')
+    path('lpusuario/', views.landingPageUsuario, name='lpUsuario'),
+    path('buscar/', views.busquedaArticulos, name='buscar'),
+    path('buscar/articulo/<int:articuloId>/', views.fichaArticulo, name='articuloEncontrado')
 ]
